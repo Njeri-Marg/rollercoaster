af1e37fe25c9a62438772e8d2f0f0d5ad744e657
Margret Njeri <49436243+Njeri-Marg@users.noreply.github.com>
2021-12-06 10:31:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if height>= 120:
+ print("AAAAh tall, dark but not very handsome, You can ride the rollercoaster though!")
+else:
+  print("Sorry short king, eat your corn and to grow your horn")
